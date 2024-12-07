63319dfbbcef26b771a478ba956c80792aa17796
Manon Gahat <magahat@made-f0Br8s12.clusters.42paris.fr>
2024-12-07 14:44:37 +0100

    cpp01 ex00 : nouveau test dans main

--- 01/ex00/src/main.cpp
+++ 01/ex00/src/main.cpp
@@ -6,21 +6,35 @@
 /*   By: magahat <magahat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 15:54:30 by magahat           #+#    #+#             */
-/*   Updated: 2024/11/28 14:50:39 by magahat          ###   ########.fr       */
+/*   Updated: 2024/12/07 14:43:58 by magahat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Zombie.hpp"
 
+/* Zombie	randomChump2(std::string name) //TEST Pour montrer diff stack et heap
+{
+	Zombie	randomChump(name);
+
+	randomChump.announce();
+	return (randomChump);
+} */
+
 int	main(void)
 {
 	Zombie	OriginalZombie("Didier");
-	Zombie	*new_zombie;
-
 	OriginalZombie.announce();
+
+	Zombie	*new_zombie;
 	new_zombie = newZombie("Pascal");
 	(*new_zombie).announce();
-	randomChump("Yves");
 	delete new_zombie;
+
+	randomChump("Yves");
+
+	// std::cout << "debut test" << std::endl;
+	// randomChump2("Yves2");
+	// std::cout << "fin test" << std::endl << std::endl;
+
 	return (0);
 }
