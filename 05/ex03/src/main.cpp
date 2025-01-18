12722f42a2c266a8db7dd7a98af2361cb497296e
Manon Gahat <magahat@paul-f5Ar3s6.clusters.42paris.fr>
2025-01-18 16:22:25 +0100

    cpp05 ex03 : main modifie / verifier ou faire les catch et try

--- 05/ex03/src/main.cpp
+++ 05/ex03/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: magahat <magahat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 15:54:30 by magahat           #+#    #+#             */
-/*   Updated: 2025/01/18 16:05:37 by magahat          ###   ########.fr       */
+/*   Updated: 2025/01/18 16:15:44 by magahat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,58 +57,6 @@ int main( void )
 		std::cerr << "\e[1;31m" << e.what() << "\e[0m" << std::endl;
 	}
 	std::cout << std::endl;
-
-	/* 
-	std::cout << "\e[1;32m=========== Test execute avant signer ===========\e[0m" << std::endl;
-	try {
-		Bureaucrat	officer("Manon", 1);
-		ShrubberyCreationForm	paper("Home");
-		std::cout << officer << std::endl;
-		std::cout << paper << std::endl;
-		officer.executeForm(paper);
-	}
-	catch (AForm::GradeTooHighException &e) {
-		std::cerr << "\e[1;31mGrade too high : TooHighException.\e[0m" << std::endl;
-	}
-	catch (AForm::GradeTooLowException &e) {
-		std::cerr << "\e[1;31mGrade too low : TooLowException.\e[0m" << std::endl;
-	}
-	std::cout << std::endl;
-
-
-	std::cout << "\e[1;32m=========== Test too low to sign ===========\e[0m" << std::endl;
-	try {
-		Bureaucrat	officer("Manon", 150);
-		ShrubberyCreationForm	paper("Home");
-		std::cout << officer << std::endl;
-		std::cout << paper << std::endl;
-		officer.signForm(paper);
-		officer.executeForm(paper);
-	}
-	catch (AForm::GradeTooHighException &e) {
-		std::cerr << "\e[1;31mGrade too high : TooHighException.\e[0m" << std::endl;
-	}
-	catch (AForm::GradeTooLowException &e) {
-		std::cerr << "\e[1;31mGrade too low : TooLowException.\e[0m" << std::endl;
-	}
-	std::cout << std::endl;
-	
-	std::cout << "\e[1;32m=========== Test too low to execute ===========\e[0m" << std::endl;
-	try {
-		Bureaucrat	officer("Manon", 144);
-		ShrubberyCreationForm	paper("Home");
-		std::cout << officer << std::endl;
-		std::cout << paper << std::endl;
-		officer.signForm(paper);
-		officer.executeForm(paper);
-	}
-	catch (Bureaucrat::GradeTooHighException &e) {
-		std::cerr << "\e[1;31mGrade too high : TooHighException.\e[0m" << std::endl;
-	}
-	catch (Bureaucrat::GradeTooLowException &e) {
-		std::cerr << "\e[1;31mGrade too low : TooLowException.\e[0m" << std::endl;
-	}
-	std::cout << std::endl; */
 
 	return (0);
 }
