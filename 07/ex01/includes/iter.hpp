0bda1ada8adb951307ca4405f47e9586c59841be
Manon Gahat <magahat@paul-f5Ar3s6.clusters.42paris.fr>
2025-01-22 12:18:47 +0100

    cpp07 ex01 ok

--- 07/ex01/includes/iter.hpp
+++ 07/ex01/includes/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: magahat <magahat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 16:34:28 by magahat           #+#    #+#             */
-/*   Updated: 2025/01/20 15:15:24 by magahat          ###   ########.fr       */
+/*   Updated: 2025/01/22 12:16:36 by magahat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,18 @@
 #define ITER_HPP
 
 #include <iostream>
-#include <cstdlib>
-#include <climits>
-#include <float.h>
 
-class Iter
-{
-	private:
-		Iter();
-		Iter(const Iter &Iter);
-		Iter &operator=(const Iter &other);
+template< typename T>
+void	iter(T* ar, size_t len, void (*fct)(const T &)) {
+	std::cout << "--------- Iter ---------" << std::endl;
+	for (size_t i = 0; i < len; i++)
+		fct(ar[i]);
 	
-	public:
-		~Iter();
-		static void convert(const std::string &s);
 };
+
+template< typename T>
+void printT(const T &element) {
+	std::cout << element << std::endl;
+}
 
 #endif
--- 07/ex01/src/main.cpp
+++ 07/ex01/src/main.cpp
@@ -6,12 +6,24 @@
 /*   By: magahat <magahat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 15:54:30 by magahat           #+#    #+#             */
-/*   Updated: 2025/01/20 15:16:15 by magahat          ###   ########.fr       */
+/*   Updated: 2025/01/22 12:17:52 by magahat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/iter.hpp"
 
 int main( void ) {
+	int i[5] = {1, 2, 3, 4, 5};
+	double	d[5] = {1.1, 2.2, 3.3, 4.4, 5.5};
+	float	f[3] = {1.1f, 2.2f, 3.3f};
+	char	c[3] = {'a', 'b', 'c'};
+	std::string str[3] = {"hello", "world", "!"};
+	void	*ptr[3] = {&i, &d, &f};
 	
+	iter(i, 5, &printT);
+	iter(d, 5, &printT);
+	iter(f, 3, &printT);
+	iter(c, 3, &printT);
+	iter(str, 3, &printT);
+	iter(ptr, 3, &printT);
 }
