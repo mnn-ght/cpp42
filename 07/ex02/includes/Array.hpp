a8c6d0e06b9d5f73695ec7de15d939073fdf2138
Manon Gahat <magahat@paul-f5Ar3s6.clusters.42paris.fr>
2025-01-22 14:22:56 +0100

    cpp07 ex02

--- 07/ex02/includes/Array.hpp
+++ 07/ex02/includes/Array.hpp
@@ -6,29 +6,67 @@
 /*   By: magahat <magahat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 16:34:28 by magahat           #+#    #+#             */
-/*   Updated: 2025/01/20 15:16:32 by magahat          ###   ########.fr       */
+/*   Updated: 2025/01/22 14:05:18 by magahat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
 #include <iostream>
-#include <cstdlib>
-#include <climits>
-#include <float.h>
+#include <exception>
+#include <sstream>
 
+template<typename T>
 class Array
 {
-	private:
-		Array();
-		Array(const Array &Array);
-		Array &operator=(const Array &other);
+private:
+	T *_ar;
+	unsigned int _len;
+public:
+	Array() : _ar(NULL), _len(0){};
+	Array(unsigned int n) : _ar(NULL), _len(n) {
+		if (n > 0)
+			_ar = new T[n];
+	};
+	Array(Array const &other) : _ar(NULL), _len(other._len){
+		_ar = new T[_len];
+		for (unsigned int i = 0; i < _len; i++)
+			_ar[i] = other._ar[i];
+	};
+
+	Array &operator=(const Array &other) {
+		if (_ar != NULL)
+			delete[] this->_ar;
+		_len = other._len;
+		_ar = new T[_len];
+		for (unsigned int i = 0; i < _len; i++)
+			_ar[i] = other._ar[i];
+		return *this;
+	};
+
+	T &operator[](const unsigned int &index) {
+		if (index >= _len)
+			throw OutOfRangeException();
+		return _ar[index];
+	};
+
+	class OutOfRangeException : public std::exception {
+		const char* what() const throw() { return "The asked index is out of range : it's higher than the length of the array.";};
+	};
+
 	
-	public:
-		~Array();
-		static void convert(const std::string &s);
+	~Array() {
+		if (_ar != NULL)
+		{
+			delete[] _ar;
+			_ar = NULL;
+		}
+	};
+
+	unsigned int size() const { return _len; };
 };
+
+
 
 #endif
--- 07/ex02/src/main.cpp
+++ 07/ex02/src/main.cpp
@@ -6,12 +6,92 @@
 /*   By: magahat <magahat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 15:54:30 by magahat           #+#    #+#             */
-/*   Updated: 2025/01/20 15:16:38 by magahat          ###   ########.fr       */
+/*   Updated: 2025/01/22 14:22:18 by magahat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Array.hpp"
 
 int main( void ) {
+	// int *a = new int();
+	// std::cout << *a << std::endl;
 	
+	Array<int> a(5);
+	Array<int> b;
+	for (size_t i = 0; i < 5; i++)
+		a[i] = i;
+	
+	std::cout << "\e[1;32m=== Array<int> a(5) ===\e[0m" << std::endl;
+	try
+	{
+		std::cout << "size a = " << a.size() << std::endl;
+		std::cout << "a[2] = " << a[2] << std::endl;
+		std::cout << "a[10] = " << a[10] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	std::cout << "\e[1;32m=== Array<int> b ===\e[0m" << std::endl;
+	try
+	{
+		std::cout << "size b = " << b.size() << std::endl;
+		std::cout << "b[2] = " << b[2] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	
+	std::cout << "\e[1;32m=== a = b ===\e[0m" << std::endl;
+	b = a;
+	try
+	{
+		std::cout << "size b = " << b.size() << std::endl;
+		std::cout << "b[2] = " << b[2] << std::endl;
+		std::cout << "b[5] = " << b[5] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	Array<int> c(a);
+	std::cout << "\e[1;32m=== Array<int> c(a) ===\e[0m" << std::endl;
+	try
+	{
+		std::cout << "size c = " << c.size() << std::endl;
+		std::cout << "c[0] = " << c[0] << std::endl;
+		std::cout << "c[2] = " << c[2] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	Array<std::string> d(5);
+	std::cout << "\e[1;32m=== Array<std::string> d(5) ===\e[0m" << std::endl;
+	for (size_t i = 0; i < 5; i++)
+	{
+		std::ostringstream	ofs;
+		ofs << "string number " << i;
+		d[i] = ofs.str();
+	}
+	try
+	{
+		std::cout << "d[2] = " << d[2] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	
+	std::cout << "\e[1;32m=== Array<std::string> e(10) && e = d ===\e[0m" << std::endl;
+	Array<std::string> e(10);
+	std::cout << "size before (e = d) = " << e.size() << std::endl;
+	e = d;
+	std::cout << "size after (e = d) = " << e.size() << std::endl;
+
+	return 0;
 }
