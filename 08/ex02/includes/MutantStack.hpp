5855c540c018f7bc63aed15d768f67955bfdb578
Manon Gahat <magahat@paul-f5Ar3s6.clusters.42paris.fr>
2025-01-26 10:54:40 +0100

    cpp08 ex02 : ok

--- 08/ex02/includes/MutantStack.hpp
+++ 08/ex02/includes/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: magahat <magahat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 16:34:28 by magahat           #+#    #+#             */
-/*   Updated: 2025/01/23 13:26:01 by magahat          ###   ########.fr       */
+/*   Updated: 2025/01/26 10:42:52 by magahat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,24 @@ public:
 	MutantStack(const MutantStack &other) : std::stack<T>(other) {};
 	~MutantStack() {};
 	MutantStack &operator=(const MutantStack &other) {
-		if ( this == &other)
-			return *this;
-		std::stack<T>::operator=(other);
+		if (this != &other)
+			std::stack<T>::operator=(other);
 		return *this;
 	};
 	
+	typedef typename std::stack<T>::container_type::iterator iterator;
+	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+	typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+	typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
 	
+	iterator begin() { return std::stack<T>::c.begin(); };
+	iterator end() { return std::stack<T>::c.end(); };
+	const_iterator begin() const { return std::stack<T>::c.begin(); };
+	const_iterator end() const { return std::stack<T>::c.end(); };
+	reverse_iterator rbegin() { return std::stack<T>::c.rbegin(); };
+	reverse_iterator rend() { return std::stack<T>::c.rend(); };
+	const_reverse_iterator rbegin() const { return std::stack<T>::c.rbegin(); };
+	const_reverse_iterator rend() const { return std::stack<T>::c.rend(); };
 };
 
 
--- 08/ex02/src/main.cpp
+++ 08/ex02/src/main.cpp
@@ -6,34 +6,62 @@
 /*   By: magahat <magahat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 15:54:30 by magahat           #+#    #+#             */
-/*   Updated: 2025/01/23 12:32:48 by magahat          ###   ########.fr       */
+/*   Updated: 2025/01/26 10:53:33 by magahat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/MutantStack.hpp"
+#include <list>
 
 int main()
 {
+	std::cout << "\e[1;32m=== TEST MUTANTSTACK ===\e[0m" << std::endl;
 	MutantStack<int> mstack;
 	mstack.push(5);
 	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
+	std::cout << "top = " << mstack.top() << std::endl;
 	mstack.pop();
-	std::cout << mstack.size() << std::endl;
+	std::cout << "size = " << mstack.size() << std::endl;
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
 	mstack.push(0);
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
 	++it;
 	--it;
+	int i = 0;
 	while (it != ite)
 	{
-		std::cout << *it << std::endl;
+		std::cout << "Number [" << i << "] = " << *it << std::endl;
 		++it;
+		i++;
 	}
+	std::cout << "top = " << mstack.top() << std::endl;
 	std::stack<int> s(mstack);
+
+	std::cout << "\e[1;32m=== TEST LIST ===\e[0m" << std::endl;
+	std::list<int> l1;
+	l1.push_back(5);
+	l1.push_back(17);
+	std::cout << "top = " << l1.back() << std::endl;
+	l1.pop_back();
+	std::cout << "size = " << l1.size() << std::endl;
+	l1.push_back(3);
+	l1.push_back(5);
+	l1.push_back(737);
+	l1.push_back(0);
+	std::list<int>::iterator lit = l1.begin();
+	std::list<int>::iterator lite = l1.end();
+	++lit;
+	--lit;
+	int j = 0;
+	while (lit != lite)
+	{
+		std::cout << "Number [" << j << "] = " << *lit << std::endl;
+		++lit;
+		j++;
+	}
+	std::cout << "top = " << l1.back() << std::endl;
 	return 0;
 }
