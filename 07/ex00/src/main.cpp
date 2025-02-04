a563bb75b5b1e79309d57c729a35a7340ebdff41
Manon Gahat <magahat@paul-f5Ar3s6.clusters.42paris.fr>
2025-02-04 15:40:56 +0100

    cpp07 ex00 : meilleur main

--- 07/ex00/src/main.cpp
+++ 07/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: magahat <magahat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 15:54:30 by magahat           #+#    #+#             */
-/*   Updated: 2025/01/22 12:03:13 by magahat          ###   ########.fr       */
+/*   Updated: 2025/02/04 15:05:47 by magahat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int main( void ) {
 	std::cout << "min( c, d ) = " << ::min( c, d ) << "\t\t\e[2;31mShould output = 'min(c, d) = chaine1'\e[0m" << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << "\t\t\e[2;31mShould output = 'max(c, d) = chaine2'\e[0m" << std::endl;
 	
-	/* int x = 1;
+	int x = 1;
 	int y = 3;
 	double d1 = 5.6;
 	double d2 = -9.4;
@@ -41,39 +41,53 @@ int main( void ) {
 
 	std::cout << std::endl;
 	std::cout << "==================== INT ====================" << std::endl;
-	std::cout << "BEFORE SWAP" << std ::endl << "x = " << x << std::endl << "y = " << y << std::endl;
+	std::cout << "BEFORE SWAP" << std ::endl << "x = " << x << ", y = " << y << std::endl;
 	::swap(x, y) ;
-	std::cout << "AFTER SWAP" << std ::endl << "x = " << x << std::endl << "y = " << y << std::endl;
+	std::cout << "AFTER SWAP" << std ::endl << "x = " << x << ", y = " << y << std::endl;
+	std::cout << "min = " << ::min( x, y ) << std::endl;
+	std::cout << "max = " << ::max( x, y ) << std::endl;
 	std::cout << std::endl;
 	std::cout << "==================== DOUBLE ====================" << std::endl;
-	std::cout << "BEFORE SWAP" << std ::endl << "d1 = " << d1 << std::endl << "d2 = " << d2 << std::endl;
+	std::cout << "BEFORE SWAP" << std ::endl << "d1 = " << d1 << ", d2 = " << d2 << std::endl;
 	::swap(d1, d2) ;
-	std::cout << "AFTER SWAP" << std ::endl << "d1 = " << d1 << std::endl << "d2 = " << d2 << std::endl;
+	std::cout << "AFTER SWAP" << std ::endl << "d1 = " << d1 << ", d2 = " << d2 << std::endl;
+	std::cout << "min = " << ::min( d1, d2 ) << std::endl;
+	std::cout << "max = " << ::max( d1, d2 ) << std::endl;
 	std::cout << std::endl;
 	std::cout << "==================== FLOAT ====================" << std::endl;
-	std::cout << "BEFORE SWAP" << std ::endl << "f1 = " << f1 << std::endl << "f2 = " << f2 << std::endl;
+	std::cout << "BEFORE SWAP" << std ::endl << "f1 = " << f1 << ", f2 = " << f2 << std::endl;
 	::swap(f1, f2) ;
-	std::cout << "AFTER SWAP" << std ::endl << "f1 = " << f1 << std::endl << "f2 = " << f2 << std::endl;
+	std::cout << "AFTER SWAP" << std ::endl << "f1 = " << f1 << ", f2 = " << f2 << std::endl;
+	std::cout << "min = " << ::min( f1, f2 ) << std::endl;
+	std::cout << "max = " << ::max( f1, f2 ) << std::endl;
 	std::cout << std::endl;
 	std::cout << "==================== CHAR ====================" << std::endl;
-	std::cout << "BEFORE SWAP" << std ::endl << "c1 = " << c1 << std::endl << "c2 = " << c2 << std::endl;
+	std::cout << "BEFORE SWAP" << std ::endl << "c1 = " << c1 << ", c2 = " << c2 << std::endl;
 	::swap(c1, c2) ;
-	std::cout << "AFTER SWAP" << std ::endl << "c1 = " << c1 << std::endl << "c2 = " << c2 << std::endl;
+	std::cout << "AFTER SWAP" << std ::endl << "c1 = " << c1 << ", c2 = " << c2 << std::endl;
+	std::cout << "min = " << ::min( c1, c2 ) << std::endl;
+	std::cout << "max = " << ::max( c1, c2 ) << std::endl;
 	std::cout << std::endl;
 	std::cout << "==================== STRING ====================" << std::endl;
-	std::cout << "BEFORE SWAP" << std ::endl << "s1 = " << s1 << std::endl << "s2 = " << s2 << std::endl;
+	std::cout << "BEFORE SWAP" << std ::endl << "s1 = " << s1 << ", s2 = " << s2 << std::endl;
 	::swap(s1, s2) ;
-	std::cout << "AFTER SWAP" << std ::endl << "s1 = " << s1 << std::endl << "s2 = " << s2 << std::endl;
+	std::cout << "AFTER SWAP" << std ::endl << "s1 = " << s1 << ", s2 = " << s2 << std::endl;
+	std::cout << "min = " << ::min( s1, s2 ) << std::endl;
+	std::cout << "max = " << ::max( s1, s2 ) << std::endl;
 	std::cout << std::endl;
 	std::cout << "==================== PTR ====================" << std::endl;
-	std::cout << "BEFORE SWAP" << std ::endl << "ptr1 = " << ptr1 << std::endl << "ptr2 = " << ptr2 << std::endl;
+	std::cout << "BEFORE SWAP" << std ::endl << "ptr1 = " << ptr1 << ", ptr2 = " << ptr2 << std::endl;
 	::swap(ptr1, ptr2) ;
-	std::cout << "AFTER SWAP" << std ::endl << "ptr1 = " << ptr1 << std::endl << "ptr2 = " << ptr2 << std::endl;
+	std::cout << "AFTER SWAP" << std ::endl << "ptr1 = " << ptr1 << ", ptr2 = " << ptr2 << std::endl;
+	std::cout << "min = " << ::min( ptr1, ptr2 ) << std::endl;
+	std::cout << "max = " << ::max( ptr1, ptr2 ) << std::endl;
 
 	std::cout << "==================== VALUE PTR ====================" << std::endl;
-	std::cout << "BEFORE SWAP" << std ::endl << "*ptr1 = " << *ptr1 << std::endl << "*ptr2 = " << *ptr2 << std::endl;
+	std::cout << "BEFORE SWAP" << std ::endl << "*ptr1 = " << *ptr1 << ", *ptr2 = " << *ptr2 << std::endl;
 	::swap(*ptr1, *ptr2) ;
-	std::cout << "AFTER SWAP" << std ::endl << "*ptr1 = " << *ptr1 << std::endl << "*ptr2 = " << *ptr2 << std::endl; */
+	std::cout << "AFTER SWAP" << std ::endl << "*ptr1 = " << *ptr1 << ", *ptr2 = " << *ptr2 << std::endl;
+	std::cout << "min = " << ::min( *ptr1, *ptr2 ) << std::endl;
+	std::cout << "max = " << ::max( *ptr1, *ptr2 ) << std::endl;
 	
 	return 0;
 }
